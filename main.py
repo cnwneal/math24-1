20208bb67a15feb13df55c57e2890a38f8a7aab3
Robert Brandt <r.e.brandt@gmail.com>
2016-10-17 19:02:35 -0400

    solution using global data structure

--- main.py
+++ main.py
@@ -16,35 +16,30 @@ def getSolutions(nums):
     global solvedOrders
     operators = set([op.add,op.sub,op.mul,op.div])
     uniqueOperatorSets = list(itertools.permutations(operators,3))
-    solutions = []
     for opSet in uniqueOperatorSets:
         for numbers in itertools.permutations(nums):
             numbers = tuple(numbers)
-            if (opSet,numbers) in solvedOrders:
-                assert 'hit it'
+            if (numbers,opSet) in solvedOrders:
                 continue
             try:
                 a = opSet[0](numbers[0],numbers[1])
                 b = opSet[1](numbers[2],numbers[3])
                 if opSet[2](a,b) == 24:
                     #print('%s %s' % (numbers,len(solvedOrders)))
-                    solvedOrders.add((opSet,numbers))
-                    solutions.append((opSet,numbers))
+                    solvedOrders.add((numbers,opSet))
             except ZeroDivisionError:
                 continue
-    return solutions
+    return solvedOrders
 
 def main():
-    count = 0
+    global solvedOrders
     for number in xrange(10000):
-        numbers = [number/1000,(number/100)%10,(number/10)%10,number%10]
+        numbers = [(number/1000)%10,(number/100)%10,(number/10)%10,number%10]
         #print('%s %s' % (number,numbers))
-        solutions = getSolutions(numbers)
-        for opSet,nums in solutions:
-            print('(%s%s%s)%s(%s%s%s) == 24' % (nums[0],operatorDisplay(opSet[0]),nums[1],operatorDisplay(opSet[2]),nums[2],operatorDisplay(opSet[1]),nums[3]))
-        if len(solutions) > 0:
-            count += 1
-    print(count)
+        getSolutions(numbers)
+    for nums,opSet in solvedOrders:
+        print('(%s%s%s)%s(%s%s%s) == 24' % (nums[0],operatorDisplay(opSet[0]),nums[1],operatorDisplay(opSet[2]),nums[2],operatorDisplay(opSet[1]),nums[3]))
+    print(len(solvedOrders))
 
 if __name__ == '__main__':
     main()
