d026280194f569ae4ef1da5134a0d9d20917ad6d
Robert Brandt <r.e.brandt@gmail.com>
2016-10-18 19:01:38 -0400

    actually honor min/max values with combinations

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def getSolutions(nums,solvedOrders={}):
                 a = opSet[0](numbers[0],numbers[1])
                 b = opSet[1](numbers[2],numbers[3])
                 if opSet[2](a,b) == 24:
-                    #print('%s %s' % (numbers,len(solvedOrders)))
                     solvedOrders.setdefault(nums,set())
                     solvedOrders[nums].add((numbers,opSet))
             except ZeroDivisionError:
@@ -30,23 +29,32 @@ def getSolutions(nums,solvedOrders={}):
     return solvedOrders
 
 def main():
-    solvedOrders = {}
     # dict of solutions to optimize and deduplicate
     # keys are tuples of numbers, value is set of operation orders
-    count = 0
-    uniqueNums = set() # avoid same number set showing in diff orders
+    solvedOrders = {}
+    # avoid same number set showing in diff orders
     # 0, 0, 4, 6 was first example hit, generated in multiple orders
-    for number in xrange(10000):
-        numbers = ( (number/1000)%10,(number/100)%10,(number/10)%10,number%10 )
+    uniqueNums = set()
+    # tally unique solutions
+    count = 0
+    for n1,n2,n3,n4 in itertools.combinations_with_replacement(xrange(MIN_VALUE,MAX_VALUE),4):
+        numbers = ( n1,n2,n3,n4 )
         if tuple(sorted(numbers)) in uniqueNums:
             continue
         uniqueNums.add(tuple(sorted(numbers)))
-        #print('%s %s' % (number,numbers))
         getSolutions(numbers,solvedOrders)
     for nums in sorted(solvedOrders.keys()):
         print(str(nums))
         for numbers,opSet in solvedOrders[nums]:
-            print('\t(%s%s%s)%s(%s%s%s) == 24' % (numbers[0],operatorDisplay(opSet[0]),numbers[1],operatorDisplay(opSet[2]),numbers[2],operatorDisplay(opSet[1]),numbers[3]))
+            print('\t(%s%s%s)%s(%s%s%s) == 24' % (
+                numbers[0],
+                operatorDisplay(opSet[0]),
+                numbers[1],
+                operatorDisplay(opSet[2]),
+                numbers[2],
+                operatorDisplay(opSet[1]),
+                numbers[3]
+            ))
             count += 1
     print('\nThere are %s solutions' % count)
 
